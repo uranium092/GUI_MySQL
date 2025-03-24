717788acb111cacc995f262fe37da875572e1b14
esneider-77 <practicante.it1@oktaxiatlanta.com>
2025-03-24 18:16:17 -0500

    detect changes for cell and update

--- src/Universal/Bridge.java
+++ src/Universal/Bridge.java
@@ -5,35 +5,39 @@ import java.sql.*;
 import java.util.*;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 
 public class Bridge {
 	private JComboBox<String>menu;
-	Interface interFace;
-	JPanel tableContainer;
-	Connection connection;
-	PreparedStatement statementToDelete;
+	private Interface interfaceComponent;
+	private JPanel tableContainer;
+	private Connection connection;
+	private Statement statamentOperations;
 	
 	public Bridge(Interface i){
-		
-		interFace=i;
-		tableContainer=interFace.getTableContainer();
-		menu=interFace.getMenu();
+		interfaceComponent=i;
+		tableContainer=interfaceComponent.getTableContainer();
+		menu=interfaceComponent.getMenu();
 		connection=new ConnectionDB().getConnection();
 		try {
-			statementToDelete=connection.prepareStatement("SELECT * FROM test");
+			statamentOperations=connection.createStatement();
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		fillItemsMenu();
 		menu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				
 				displayTable();
 			}
 		});
 		
 		
-		interFace.getDeleteButton().addActionListener(new ActionListener() {
+		interfaceComponent.getDeleteButton().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				try {
 					
@@ -46,7 +50,7 @@ public class Bridge {
 		});
 				
 	}
-	public void fillItemsMenu(){
+	private void fillItemsMenu(){
 		ResultSet tables=null;
 		try {
 			tables=connection.getMetaData().getTables(connection.getCatalog(), null, null, null);
@@ -59,17 +63,16 @@ public class Bridge {
 		}
 	}
 	private JTable table;
-	private ArrayList< ArrayList<String> > recordsStorage=new ArrayList< ArrayList<String> >();
 	private String keyField;
-	public void displayTable(){
+	private void displayTable(){
 		try {
-			recordsStorage.clear();
 			String sql="SELECT * FROM "+(String)menu.getSelectedItem();
 			Statement st=connection.createStatement();
 			ResultSet records=st.executeQuery(sql);
-			ResultSetMetaData metaData=records.getMetaData(); // ResultSet metadatos de la salida de la consulta
+			ResultSetMetaData metaData=records.getMetaData();
 			String[][] matrixRecords=new String[getRecordsCount(sql)][metaData.getColumnCount()];
 			int row=0;
+			ArrayList< ArrayList<String> > recordsStorage=new ArrayList< ArrayList<String> >();
 			while(records.next()) {
 				ArrayList<String> temporalArrayList=new ArrayList<String>();
 				for(int col=0;col<metaData.getColumnCount();col++) {
@@ -87,7 +90,27 @@ public class Bridge {
 				colm[col]=metaData.getColumnLabel(col+1);
 				if(col==0) keyField=colm[0];
 			}
-			table=new JTable(matrixRecords,colm);
+			table=new JTable(new DefaultTableModel(matrixRecords, colm) {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+			    public boolean isCellEditable(int row, int column) {
+			        return column != 0;
+			    }
+			});
+			table.getModel().addTableModelListener(new TableModelListener() {
+				
+				@Override
+				public void tableChanged(TableModelEvent e) {
+					// TODO Auto-generated method stub
+					String sql="UPDATE "+menu.getSelectedItem()+" SET "+colm[e.getColumn()]+" = '"+table.getValueAt(e.getFirstRow(),e.getColumn())+"' WHERE "+keyField+"='"+table.getValueAt(e.getFirstRow(), 0)+"'";
+					try {
+						statamentOperations.execute(sql);
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+			});
 			
 			tableContainer.removeAll();
 			
@@ -100,7 +123,7 @@ public class Bridge {
 		}
 	}
 	
-	public int getRecordsCount(String sql) throws SQLException{
+	private int getRecordsCount(String sql) throws SQLException{
 		Statement st=connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
 		ResultSet rs=st.executeQuery(sql);
 		rs.last();
@@ -110,16 +133,16 @@ public class Bridge {
 		return current; 
 	}
 	
-	public void deleteRecordsDB() throws SQLException{
+	private void deleteRecordsDB() throws SQLException{
 		if(table!=null) {
 			int[] indexSelectedRows=table.getSelectedRows();
 			if(indexSelectedRows.length>0) {
 				String sql="DELETE FROM "+menu.getSelectedItem()+" WHERE";
 				for(int x=0;x<indexSelectedRows.length;x++) {
-					sql+=" "+keyField+"='"+recordsStorage.get(indexSelectedRows[x]).get(0)+"' OR";
+					sql+=" "+keyField+"='"+table.getValueAt(indexSelectedRows[x], 0)+"' OR";
 				}
 				sql=sql.substring(0, sql.length()-2);
-				statementToDelete.execute(sql);
+				statamentOperations.execute(sql);
 				displayTable();
 			}
 		}
